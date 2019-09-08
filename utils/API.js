bb2527f56e7dd4a258ec91fcc7c94521bb814031
Matt Gilliland <mgilliland23@gmail.com>
2019-09-07 23:33:07 -0400

    communicating with upself API and appending to chat

---
+++ utils/API.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+function getMessageAnalysis(message) {
+  console.log('making axios req');
+  return axios
+    .post('/api/AnalyzeMessage', {
+      params: {userMessage: message},
+    })
+    .then(
+      response => {
+        console.log(response.data);
+      },
+      error => {
+        console.log(error);
+      },
+    );
+}
+
+function getMessageJaro(message) {
+  console.log('making api request...');
+  return axios
+    .post('http://localhost:80/api/jaroMessage', {
+      params: {userMessage: message},
+    })
+    .then(
+      response => {
+        console.log(response.data);
+        return response.data;
+      },
+      error => {
+        console.log(error);
+      },
+    );
+}
+
+export default {getMessageAnalysis, getMessageJaro};
