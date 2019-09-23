73651fd8bab606581fee30ec83bb82578a6d8b19
esdalee <es.da.lee@gmail.com>
2019-09-22 23:01:09 -0400

    text shows up hurray

--- components/DassResponse/index.js
+++ components/DassResponse/index.js
@@ -2,6 +2,9 @@ import React, {Component, useState} from 'react';
 import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+  },
   buttons: {
     flex: 1,
     marginTop: 10,
@@ -22,35 +25,44 @@ export default class DassResponse extends Component {
   render() {
     console.info(this.props);
     return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={[styles.buttons, {backgroundColor: 'lightgreen'}]}
-          onPress={() => this.props.handleResponse(0)}>
-          <Text style={styles.responseText}>Does not apply to me at all</Text>
-          {/* <Button title="Does not apply to me at all" /> */}
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.buttons, {backgroundColor: '#6DCEF4'}]}
-          onPress={() => this.props.handleResponse(1)}>
-          <Text style={styles.responseText}>Applies to me sometimes</Text>
-          {/* <Button title="Does not apply to me at all" /> */}
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.buttons, {backgroundColor: 'yellow'}]}
-          onPress={() => this.props.handleResponse(2)}>
-          <Text style={styles.responseText}>
-            Applies to me good amount of times
-          </Text>
-          {/* <Button title="Does not apply to me at all" /> */}
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.buttons, {backgroundColor: '#F46DCE'}]}
-          onPress={() => this.props.handleResponse(3)}>
-          <Text style={styles.responseText}>
-            Applies to me most of the time
-          </Text>
-          {/* <Button title="Does not apply to me at all" /> */}
-        </TouchableOpacity>
+      <View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.buttons, {backgroundColor: 'lightgreen'}]}
+            onPress={() => this.props.handleResponse(0)}>
+            <Text style={styles.responseText}>Does not apply to me at all</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.buttons, {backgroundColor: '#6DCEF4'}]}
+            onPress={() => this.props.handleResponse(1)}>
+            <Text style={styles.responseText}>Applies to me sometimes</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.buttons, {backgroundColor: 'yellow'}]}
+            onPress={() => this.props.handleResponse(2)}>
+            <Text style={styles.responseText}>
+              Applies to me good amount of times
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.buttons, {backgroundColor: '#F46DCE'}]}
+            onPress={() => this.props.handleResponse(3)}>
+            <Text style={styles.responseText}>
+              Applies to me most of the time
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {/* <Button
+          title="Does not apply to me at all"
+          value={0}
+          onPress={() => this.props.handleResponse(0)}
+        /> */}
         {/* <Button
           title="Applies to me sometimes"
           value={1}
